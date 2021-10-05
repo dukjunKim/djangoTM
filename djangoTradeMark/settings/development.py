f9d661530bde7e8a50b40d66d566dc3e2224da0a
dukjunKim <dj.kim@lipis.kr>
2021-10-05 16:19:22 +0900

    1st django TM git commit 2nd - Dockerfile added 3rd - name little change 4th - little change 5rd - settings seperated

---
+++ djangoTradeMark/settings/development.py
@@ -0,0 +1,6 @@
+import os
+from .base import *
+
+SECRET_KEY = 'django-insecure-^1%f2hwzjnyorh_ue=3^k!**uuz#+l-soy%^1jy51zqlnccvtr'
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
